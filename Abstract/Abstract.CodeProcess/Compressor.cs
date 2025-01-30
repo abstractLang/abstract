c5f70d7d02983d8a3f2643705efb3f107ff1728e
lumi2021 <4462076+lumi2021@users.noreply.github.com>
2025-01-29 23:46:43 -0300

    compressor things

--- Abstract/Abstract.CodeProcess/Compressor.cs
+++ Abstract/Abstract.CodeProcess/Compressor.cs
@@ -1,7 +1,6 @@
 using Abstract.Binutils.Abs.Elf;
 using Abstract.Build;
 using Abstract.CompileTarget.Core.Enums;
-using Abstract.Parser.Core.ProgData;
 using Abstract.Parser.Core.ProgMembers;
 using static Abstract.Binutils.Abs.Elf.ElfBuilder;
 
@@ -9,11 +8,11 @@ namespace Abstract.Parser;
 
 public class Compressor(ErrorHandler errHandler)
 {
-
     private readonly ErrorHandler _errHandler = errHandler;
 
     private Project _currentProject = null!;
     private ElfBuilder _builder = null!;
+    private Dictionary<ProgramMember, DirBuilder> _memberDirectoryMap = null!;
 
     public ElfProgram[] DoCompression(ProgramRoot program, ExpectedELFFormat expectedElfFormat)
     {
@@ -46,73 +45,140 @@ public class Compressor(ErrorHandler errHandler)
 
         _builder = elfBuilder;
         _currentProject = project;
+        _memberDirectoryMap = [];
 
         var projectdir = new DirectoryBuilder("PROJECT", project.identifier);
         //var includedir = new DirectoryBuilder("INCLUDE", "./");
 
         dirRoot.AppendChild(projectdir);
 
-        CompressAllChildren(project, projectdir);
+        ScanAllChildren(project, projectdir);
+        CompressAllMembers();
 
         if (project == program.MainProject) elfBuilder.hasentrypoint = true;
 
+        _memberDirectoryMap = null!;
         _currentProject = null!;
         _builder = null!;
 
         return elfBuilder;
     }
 
-    private void CompressAllChildren(ProgramMember parent, DirBuilder parentDir)
+    #region Scan
+    private void ScanAllChildren(ProgramMember parent, DirBuilder parentDir)
     {
-        foreach (var i in parent.ChildrenNamespaces) CompressNamespace(i, parentDir);
-        foreach (var i in parent.ChildrenFields) CompressFields(i, parentDir);
-        foreach (var i in parent.ChildrenFunctions) CompressFunctionGroup(i, parentDir);
-        foreach (var i in parent.ChildrenTypes) CompressStructure(i, parentDir);
-        foreach (var i in parent.ChildrenEnums) CompressEnum(i, parentDir);
+        foreach (var i in parent.ChildrenNamespaces) ScanNamespace(i, parentDir);
+        foreach (var i in parent.ChildrenFields) ScanField(i, parentDir);
+        foreach (var i in parent.ChildrenFunctions) ScanFunctionGroup(i, parentDir);
+        foreach (var i in parent.ChildrenTypes) ScanStructure(i, parentDir);
+        foreach (var i in parent.ChildrenEnums) ScanEnum(i, parentDir);
     }
 
-
-    private void CompressNamespace(Namespace member, DirBuilder parent)
+    private void ScanNamespace(Namespace member, DirBuilder parent)
     {
         var header = new DirectoryBuilder("NMSP", member.identifier);
+        _memberDirectoryMap.Add(member, header);
 
-        CompressAllChildren(member, header);
+        ScanAllChildren(member, header);
 
         parent.AppendChild(header);
     }
-    private void CompressFields(Field member, DirBuilder parent)
+    private void ScanField(Field member, DirBuilder parent)
     {
         var header = new DirectoryBuilder("FIELD", member.identifier);
+        _memberDirectoryMap.Add(member, header);
 
         parent.AppendChild(header);
     }
-    private void CompressFunctionGroup(FunctionGroup member, DirBuilder parent)
+    private void ScanFunctionGroup(FunctionGroup member, DirBuilder parent)
     {
-        var header = new DirectoryBuilder("FGROUP", member.identifier);
+        if (member.IsSingle)
+        {
+            ScanFunction(member.Overloads[0], parent);
+        }
+        else
+        {
+            var header = new DirectoryBuilder("FGROUP", member.identifier);
+            _memberDirectoryMap.Add(member, header);
 
+            for (var i = 0; i < member.Overloads.Length; i++)
+                ScanFunction(member.Overloads[i], header, i);
 
-        parent.AppendChild(header);
+            parent.AppendChild(header);
+        }
     }
-    private void CompressFunction(Function member, DirBuilder parent)
+    private void ScanFunction(Function member, DirBuilder parent, int overloadIdx = -1)
     {
-        var header = new DirectoryBuilder("FUNC", member.identifier);
-
+        var header = new DirectoryBuilder("FUNC", 
+            overloadIdx >= 0 ? $"#{overloadIdx:X}" : member.identifier);
+        _memberDirectoryMap.Add(member, header);
 
         parent.AppendChild(header);
     }
-    private void CompressStructure(Structure member, DirBuilder parent)
+    private void ScanStructure(Structure member, DirBuilder parent)
     {
         var header = new DirectoryBuilder("TYPE", member.identifier);
 
-        CompressAllChildren(member, header);
+        ScanAllChildren(member, header);
+        _memberDirectoryMap.Add(member, header);
 
         parent.AppendChild(header);
     }
-    private void CompressEnum(Enumerator member, DirBuilder parent)
+    private void ScanEnum(Enumerator member, DirBuilder parent)
     {
         var header = new DirectoryBuilder("ENUM", member.identifier);
-
+        _memberDirectoryMap.Add(member, header);
 
         parent.AppendChild(header);
     }
+    #endregion
+
+    #region Compress
+    private void CompressAllMembers()
+    {
+        foreach (var i in _memberDirectoryMap)
+        {
+            if (i.Key is Namespace @a) CompressNamespace(a, i.Value);
+            else if (i.Key is Field @b) CompressField(b, i.Value);
+            else if (i.Key is FunctionGroup @c) CompressFunctionGroup(c, i.Value);
+            else if (i.Key is Function @d) CompressFunction(d, i.Value);
+            else if (i.Key is Structure @e) CompressStructure(e, i.Value);
+            else if (i.Key is Enumerator @f) CompressEnum(f, i.Value);
+        }
+    }
+
+    // These two are really basic lol
+    private void CompressNamespace(Namespace member, DirBuilder dir)
+    {
+        AppendGlobal(member, dir);
+    }
+    private void CompressFunctionGroup(FunctionGroup member, DirBuilder dir)
+    {
+        AppendGlobal(member, dir);
+    }
+
+    // Complex things after here
+    private void CompressField(Field member, DirBuilder dir)
+    {
+        AppendGlobal(member, dir);
+    }
+    private void CompressFunction(Function member, DirBuilder dir)
+    {
+        AppendGlobal(member, dir);
+    }
+    private void CompressStructure(Structure member, DirBuilder dir)
+    {
+        AppendGlobal(member, dir);
+    }
+    private void CompressEnum(Enumerator member, DirBuilder dir)
+    {
+        AppendGlobal(member, dir);
+    }
+
+    private void AppendGlobal(ProgramMember member, DirBuilder dir)
+    {
+        var global = new DirectoryBuilder("GLOBAL", member.GlobalReference);
+        dir.AppendChild(global);
+    }
+    #endregion
 }
